adf264445102b165543229393df12974b58f0cb9
RahulNeupane <nrahul503@gmail.com>
2022-08-10 22:31:20 +0545

    drawing app

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
+
 const increaseBtn = document.getElementById('increase')
 const decreaseBtn = document.getElementById('decrease')
 const sizeEl = document.getElementById('size')
 const colorEl = document.getElementById('color')
 const clearEl = document.getElementById('clear')
 
-let size = 20
-let isPressed = false
-colorEl.value = 'black'
+let size = 15
+colorEl.value = 'red'
 let color = colorEl
+let isClicked = false
 let x
 let y
 
 canvas.addEventListener('mousedown',(e)=>{
-    isPressed=true
+    isClicked = true
 
     x = e.offsetX
     y = e.offsetY
-
 })
 
-
 canvas.addEventListener('mouseup',(e)=>{
-    isPressed=false
+    isClicked = false
 
     x = undefined
     y = undefined
-
 })
-
 canvas.addEventListener('mousemove',(e)=>{
-    if(isPressed){
-        const x2 = e.offsetX
-        const y2 = e.offsetY
+    if(isClicked){
+        x2 = e.offsetX
+        y2 = e.offsetY
 
         darwCircle(x2,y2)
         drawLine(x,y,x2,y2)
@@ -41,12 +38,11 @@ canvas.addEventListener('mousemove',(e)=>{
         x = x2
         y = y2
     }
-
 })
 
 function darwCircle(x,y){
     ctx.beginPath()
-    ctx.arc(x,y , size, 0, Math.PI * 2)
+    ctx.arc(x,y,size,0,Math.PI * 2)
     ctx.fillStyle = color
     ctx.fill()
 }
@@ -60,30 +56,27 @@ function drawLine(x1,y1,x2,y2){
     ctx.stroke()
 }
 
-function updateSizeOnScreen(){
-    sizeEl.innerText = size;
+function updateSize(){
+    sizeEl.innerText = size
 }
 
 increaseBtn.addEventListener('click',()=>{
-    size += 5
+    size +=5
+
     if(size>50){
         size = 50
     }
-
-    updateSizeOnScreen()
+    updateSize()
 })
 
 decreaseBtn.addEventListener('click',()=>{
-    size -=5;
+    size -=5
 
     if(size<5){
-        size = 5;
+        size = 5
     }
-
-    updateSizeOnScreen()
+    updateSize()
 })
 
-
-colorEl.addEventListener('change',(e)=>color = e.target.value)
-
-clearEl.addEventListener('click',()=> ctx.clearRect(0,0,canvas.width,canvas.height))
+colorEl.addEventListener('change', (e)=>color = e.target.value)
+clearEl.addEventListener('click',(e)=> ctx.clearRect(0,0,canvas.width,canvas.height))
